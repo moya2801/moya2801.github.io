452cb6e2c1b737f21081a70aa67d91fd3e1481b9
moya2801 <pablolopezcabezas9@gmail.com>
2025-05-20 20:01:05 +0200

    Actualizado Java Script

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.getElementById("darkModeToggle").addEventListener("click", Modo);
+
+function Modo() {
+    document.body.classList.toggle("dark-mode");
+};
+
+document.getElementById("verMasBtn").addEventListener("click",VerMas);
+
+ function VerMas() {
+    const texto = document.getElementById("textoExtra");
+    texto.style.display = texto.style.display === "none" ? "block" : "none";
+}
